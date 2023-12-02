f808849c223f6784dd9a1a7c8cf6e13728ef1a2b
ErexPL <zys.dawid@gmail.com>
2023-12-02 11:58:07 +0100

    Sliding animation

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 let firstRequestBool = true;
+let animation = null;
 
 function sendRequest() {
   const xhttp = new XMLHttpRequest();
@@ -52,22 +53,38 @@ function followingRequest(xml) {
   const main = document.getElementById('main');
   main.innerHTML += '<section id="3"><p id="option3"></p></section>';
 
-  let p2 = document.getElementById("option2");
-  let p3 = document.getElementById("option3");
-  let sectionLeft = document.getElementById("1");
-  let sectionMiddle = document.getElementById("2");
-  let sectionRight = document.getElementById("3");
-
   let itemIndex = Math.floor(Math.random() * x.length);
   let baseName3 = x[itemIndex].getElementsByTagName("baseName")[0].childNodes[0].nodeValue;
   let targetName3 = x[itemIndex].getElementsByTagName("targetName")[0].childNodes[0].nodeValue;
   let exchangeRate3 = x[itemIndex].getElementsByTagName("exchangeRate")[0].childNodes[0].nodeValue;
   let text3 = baseName3 + " --> " + targetName3 + " | " + exchangeRate3;
   document.getElementById("option3").innerHTML = text3;
-  
-  sectionLeft.remove();
-  sectionMiddle.id = "1";
-  p2.id = "option1";
-  sectionRight.id = "2";
-  p3.id = "option2";
+
+  slideLeft(main);
+}
+
+function slideLeft(main) {;
+  let p2 = document.getElementById("option2");
+  let p3 = document.getElementById("option3");
+  let sectionLeft = document.getElementById("1");
+  let sectionMiddle = document.getElementById("2");
+  let sectionRight = document.getElementById("3");
+
+  let pos = 0;
+  clearInterval(animation);
+  animation = setInterval(frame, 10);
+  function frame() {
+    if (pos == -50) {
+      main.style.left = 0;
+      sectionLeft.remove();
+      sectionMiddle.id = "1";
+      p2.id = "option1";
+      sectionRight.id = "2";
+      p3.id = "option2";
+      clearInterval(animation); 
+    } else {
+      pos--;
+      main.style.left = pos + '%';
+    }
+  }
 }
